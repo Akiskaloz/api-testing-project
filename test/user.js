cd9d8fbf0876ed69659f445f1a6109732915b48d
Akiskaloz <reklamszemet@gmail.com>
2022-09-22 21:17:07 +0100

    callback added to improve reliabilty

--- test/user.js
+++ test/user.js
@@ -4,10 +4,11 @@ const request = supertest('https://gorest.co.in//public-api/');
 const TOKEN = "8f772e0a32f084370ac1c590dab582baced6cc2d799a2f97fbefbfeaa434ddaf";
 
 describe('Users', () => {
-    it('GET /users', () => {
+    it('GET /users', (done) => {
         request
             .get(`users?acces-token=${TOKEN}`).end((err,res) => {
                 expect(res.body.data).to.not.be.empty;
+                done();     // callback to done, so negative test wouldn't fail.
             });
     });
 });
