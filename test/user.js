864c40189ff3253332c248520a9fb0b00027866f
Akiskaloz <reklamszemet@gmail.com>
2022-09-22 18:11:33 +0100

    Initial commit

---
+++ test/user.js
@@ -0,0 +1,13 @@
+import supertest from "supertest";
+import { expect } from "chai";
+const request = supertest('https://gorest.co.in//public-api/');
+const TOKEN = "8f772e0a32f084370ac1c590dab582baced6cc2d799a2f97fbefbfeaa434ddaf";
+
+describe('Users', () => {
+    it('GET /users', () => {
+        request
+            .get(`users?acces-token=${TOKEN}`).end((err,res) => {
+                expect(res.body.data).to.not.be.empty;
+            });
+    });
+});
